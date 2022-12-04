9a64ee920c767533714bb93ff5846aa1400d5d87
Zvika <zvika@xmpie.com>
2022-12-04 16:56:40 +0200

    color, center align and adding line break

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
       <v-container>
         <v-row justify="center" class="text-center" align="center">
           <v-col cols="12" align-self="center">
-            <v-card>
-              <v-card-title justify="center" class="text-center" align="center"
+            <v-card style="background: #ECEAE1;">
+              <v-card-title class="justify-center"
                 >סימולטור שכר</v-card-title
               >
-              <v-card-subtitle
+              <v-card-subtitle 
                 >מידע זה ניתן בחינם, ללא ערבות ואחריות על ידי
                 <br/>
                 <a href="https://www.morimovilim.org/"><img src="https://static.wixstatic.com/media/3c6951_dc81c8fac20a496599ed73975e468c7b~mv2.png/v1/crop/x_31,y_64,w_428,h_322/fill/w_248,h_186,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/%D7%9E%D7%95%D7%A8%D7%99%D7%9D%20%D7%9E%D7%95%D7%91%D7%99%D7%9C%D7%99%D7%9D%20%D7%9C%D7%A9%D7%99%D7%A0%D7%95%D7%99%20(3).png" />
-                לאתר מורים מובילים שינוי לחץ כאן
+                  <br/>לאתר מורים מובילים שינוי לחץ כאן
                 </a>
                 <p>
                   <a
